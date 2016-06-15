6485167e537b4ca7df8748cd017000ed4583cfcc
Rosa <ROSA@HQ.un.fao.org>
2016-06-15 16:12:53 +0200

    Input/output data validation

--- modules/seed_imputation/seed_imputation.R
+++ modules/seed_imputation/seed_imputation.R
@@ -31,6 +31,7 @@ suppressMessages({
   library(faoswsProcessing)
   library(splines)
   library(faoswsImputation)
+  library(faoswsEnsure)
   
 })
 
@@ -52,6 +53,7 @@ valuePrefix = "Value_measuredElement_"
 flagObsPrefix = "flagObservationStatus_measuredElement_"
 flagMethodPrefix = "flagMethod_measuredElement_"
 
+
 updateModel = TRUE
 
 ## set up for the test environment and parameters
@@ -65,6 +67,7 @@ if(CheckDebug()){
   SETTINGS <- ReadSettings("sws.yml")
   SetClientFiles(SETTINGS[["certdir"]])
   
+  
   GetTestEnvironment(baseUrl = SETTINGS[["server"]],
                      token = SETTINGS[["token"]])
   
@@ -74,18 +77,111 @@ if(CheckDebug()){
 
 if(updateModel){
   seed = getOfficialSeedData()
+  
+  
+
+  
+  ## Seed imput data validation
+  ## Ensure that the seed official values belong to the tha range (0, inf). 
+  ensureValueRange(data = seed,
+                   ensureColumn = "Value_measuredElement_5525",
+                   min = 0,
+                   max = Inf,
+                   includeEndPoint = TRUE,
+                   returnData = FALSE,
+                   getInvalidData = FALSE)
+  
+  
+  
+  ensureFlagValidity(data = seed,
+                     flagObservationVar = "flagObservationStatus_measuredElement_5525",
+                     flagMethodVar = "flagMethod_measuredElement_5525",
+                     returnData = FALSE,
+                     getInvalidData = TRUE)
+  
+  
+  ##Ensure   CorrectMissingValue not necessary because I pull only official data whose ObservationFlag is ""
+  
+  ensureCorrectMissingValue(data = seed,
+                            valueVar = "Value_measuredElement_5525",
+                            flagObservationStatusVar = "flagObservationStatus_measuredElement_5525",
+                            missingObservationFlag = "M",
+                            returnData = FALSE,
+                            getInvalidData = TRUE)
+  
+  
+  
   seed = removeCarryForward(data = seed,
                             variable = "Value_measuredElement_5525")
   seed = buildCPCHierarchy(data = seed, cpcItemVar = itemVar, levels = 3)
   
+  
+  
+  
+  
   area = getAllAreaData()
-  imputeAreaSown(data = area)
   
-  climate = getWorldBankClimateData()
+  areaConflict =areaRemoveZeroConflict(area,
+                         value1= "Value_measuredElement_5025",
+                         value2= "Value_measuredElement_5312",
+                         observationFlag1= "flagObservationStatus_measuredElement_5025",
+                         methodFlag1= "flagMethod_measuredElement_5025",
+                         missingObservationFlag = "M",
+                         missingMethodFlag = "u"
+                         )
   
-  finalModelData = mergeAllSeedData(seedData = seed, area, climate)
+  areaPreProcessed= preProcessing(data= areaConflict,
+                normalised = FALSE)
+  
+  ensureValueRange(data = areaPreProcessed,
+                   ensureColumn = "Value_measuredElement_5312",
+                   min = 0,
+                   max = Inf,
+                   includeEndPoint = TRUE,
+                   returnData = FALSE,
+                   getInvalidData = FALSE)
+  
+  
+  ensureValueRange(data = areaPreProcessed,
+                   ensureColumn = "Value_measuredElement_5025",
+                   min = 0,
+                   max = Inf,
+                   includeEndPoint = TRUE,
+                   returnData = FALSE,
+                   getInvalidData = FALSE)
+  
+  
+  
+ imputedArea=imputeAreaSown(data = areaPreProcessed,
+                 byKey= "geographicAreaM49")
+ 
+ 
+ ## AreaSown must be greater then or at least equal to area harvested. Check that in the AreaSown column all
+ ## the values are not lower than AreaHarvested
+ 
+  
+ensureNoConflictingAreaSownHarvested(data= imputedArea,
+                                      areaSown= "Value_measuredElement_5025",
+                                      areaHarvested=  "Value_measuredElement_5312",
+                                      returnData = FALSE,
+                                      normalised = FALSE,
+                                      getInvalidData = FALSE)
+  
+  
+ 
+climate= getWorldBankClimateData()
+  
+  
+  
+  
+
+
+  
+  
+  
+  finalModelData = mergeAllSeedData(seedData = seed, imputedArea, climate)
   finalModelData = finalModelData[Value_measuredElement_5525 > 1 &
-                                      Value_measuredElement_5025 > 1, ]
+                                      Value_measuredElement_5025 >1, ]
   ## We have to remove cases where we do not have temperature, as we cannot create
   ## a model when independent variables are missing.  The predictions would be NA
   ## anyways, and so we wouldn't be saving anything to the database if we left
@@ -114,12 +210,12 @@ if(updateModel){
 # qqline(residuals(lossLmeModel))
 }
 
-
+seed = getSelectedSeedData()
 
 seed = removeCarryForward(data = seed, variable = "Value_measuredElement_5525")
 seed = buildCPCHierarchy(data = seed, cpcItemVar = itemVar, levels = 3)
 
-finalPredictData = mergeAllSeedData(seedData = seed, area, climate)
+finalPredictData = mergeAllSeedData(seedData = seed, imputedArea, climate)
 finalPredictData = finalPredictData[Value_measuredElement_5525 > 1 &
                                         Value_measuredElement_5025 > 1, ]
 ## We have to remove cases where we do not have temperature, as we cannot create
@@ -146,6 +242,37 @@ finalPredictData[(is.na(Value_measuredElement_5525) |
                  list(predicted, "I", "e"))]
 
 finalPredictData[, predicted := NULL]
+
+
+
+##Output data validation 
+
+
+ensureValueRange(data = finalPredictData,
+                 ensureColumn = "Value_measuredElement_5525",
+                 min = 0,
+                 max = Inf,
+                 includeEndPoint = TRUE,
+                 returnData = FALSE,
+                 getInvalidData = FALSE)
+
+
+ensureFlagValidity(data = finalPredictData,
+                   flagObservationVar = "flagObservationStatus_measuredElement_5525",
+                   flagMethodVar = "flagMethod_measuredElement_5525",
+                   returnData = FALSE,
+                   getInvalidData = FALSE) 
+
+
+ensureProtectedData(data = finalPredictData,
+                    domain = "agriculture",
+                    dataset = "aproduction",
+                    flagObservationVar = "flagObservationStatus_measuredElement_5525",
+                    flagMethodVar = "flagMethod_measuredElement_5525",
+                    normalised = FALSE,
+                    returnData = FALSE,
+                    getInvalidData = FALSE
+                    )
 
 saveSeedData(data = finalPredictData)
 
