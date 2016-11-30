554af8017407ff41c8be9341cbf17bfad12cac00
Francesca Rosa <Francesca.Rosa@fao.org>
2016-11-30 18:44:37 +0100

    I added some flag managemnetboth when data are pulled from the db and when data are pushed back. In addition the flag management has been inserted in order to choose the items that have to be overwritten.

--- modules/seed_imputation/seed_imputation.R
+++ modules/seed_imputation/seed_imputation.R
@@ -67,19 +67,19 @@ if(CheckDebug()){
   SETTINGS <- ReadSettings("sws.yml")
   SetClientFiles(SETTINGS[["certdir"]])
   
-  
   GetTestEnvironment(baseUrl = SETTINGS[["server"]],
                      token = SETTINGS[["token"]])
   
 }
 
 
+sessionKey = swsContext.datasets[[1]]
 
-if(updateModel){
+
   seed = getOfficialSeedData()
   
   
-
+  
   
   ## Seed imput data validation
   ## Ensure that the seed official values belong to the tha range (0, inf). 
@@ -93,100 +93,235 @@ if(updateModel){
   
   
   
-## ensureFlagValidity(data = seed,
-##                   flagObservationVar = "flagObservationStatus_measuredElement_5525",
-##                     flagMethodVar = "flagMethod_measuredElement_5525",
-##                     returnData = FALSE,
-##                     getInvalidData = TRUE)
+  ensureFlagValidity(data = seed,
+                    flagObservationVar = "flagObservationStatus_measuredElement_5525",
+                      flagMethodVar = "flagMethod_measuredElement_5525",
+                      returnData = FALSE,
+                      getInvalidData = FALSE)
   
   
-##Ensure   CorrectMissingValue not necessary because I pull only official data whose ObservationFlag is ""
+  ##Ensure   CorrectMissingValue not necessary because I pull only protected data.
   
   ensureCorrectMissingValue(data = seed,
                             valueVar = "Value_measuredElement_5525",
                             flagObservationStatusVar = "flagObservationStatus_measuredElement_5525",
                             missingObservationFlag = "M",
                             returnData = FALSE,
-                            getInvalidData = TRUE)
+                            getInvalidData = FALSE)
   
   
   
   seed = removeCarryForward(data = seed,
                             variable = "Value_measuredElement_5525")
+  
+
+  
   seed = buildCPCHierarchy(data = seed, cpcItemVar = itemVar, levels = 3)
-  
-  
-  
-  
-  
-  area = getAllAreaData()
-  
-  areaConflict =areaRemoveZeroConflict(area,
-                         value1= "Value_measuredElement_5025",
-                         value2= "Value_measuredElement_5312",
-                         observationFlag1= "flagObservationStatus_measuredElement_5025",
-                         methodFlag1= "flagMethod_measuredElement_5025",
-                         missingObservationFlag = "M",
-                         missingMethodFlag = "u"
-                         )
-  
-  areaPreProcessed= preProcessing(data= areaConflict,
-                normalised = FALSE)
-  
-  ensureValueRange(data = areaPreProcessed,
-                   ensureColumn = "Value_measuredElement_5312",
-                   min = 0,
-                   max = Inf,
-                   includeEndPoint = TRUE,
-                   returnData = FALSE,
-                   getInvalidData = FALSE)
-  
-  
-  ensureValueRange(data = areaPreProcessed,
-                   ensureColumn = "Value_measuredElement_5025",
-                   min = 0,
-                   max = Inf,
-                   includeEndPoint = TRUE,
-                   returnData = FALSE,
-                   getInvalidData = FALSE)
-  
-  
-  
- imputedArea=imputeAreaSown(data = areaPreProcessed,
-                 byKey= "geographicAreaM49")
- 
- 
- ## AreaSown must be greater then or at least equal to area harvested. Check that in the AreaSown column all
- ## the values are not lower than AreaHarvested
  
   
-ensureNoConflictingAreaSownHarvested(data= imputedArea,
-                                     valueColumn1= "Value_measuredElement_5025",
-                                     valueColumn2=  "Value_measuredElement_5312",
-                                      returnData = FALSE,
-                                      normalised = FALSE,
-                                      getInvalidData = FALSE)
+## Ask to team B/C how to deal with (M,-) in the SeedModule
+##  seed =seed[flagObservationStatus_measuredElement_5525!="M" & flagMethod_measuredElement_5525!="-",]
   
+## AREA data
   
+
+  
+ area=getAllAreaData()
+
+ ensureValueRange(data = area,
+                  ensureColumn = "Value_measuredElement_5312",
+                  min = 0,
+                  max = Inf,
+                  includeEndPoint = TRUE,
+                  returnData = FALSE,
+                  getInvalidData = FALSE)
  
-climate= getWorldBankClimateData()
+ 
+ ensureValueRange(data = area,
+                  ensureColumn = "Value_measuredElement_5025",
+                  min = 0,
+                  max = Inf,
+                  includeEndPoint = TRUE,
+                  returnData = FALSE,
+                  getInvalidData = FALSE)
+
+ areaPreProcessed= preProcessing(data= area,
+                                 normalised = FALSE)
+ 
+ 
+ 
+ areaConflict =areaRemoveZeroConflict(areaPreProcessed,
+                                      value1= "Value_measuredElement_5025",
+                                      value2= "Value_measuredElement_5312",
+                                      observationFlag1= "flagObservationStatus_measuredElement_5025",
+                                      methodFlag1= "flagMethod_measuredElement_5025",
+                                      missingObservationFlag = "M",
+                                      missingMethodFlag = "u"
+ )
+ 
+
+## We need and authocorrection function that correct the invalid flag combinations
+ 
+ 
+
+areaConflictNormalised=normalise(areaConflict)
+##invalid=   ensureFlagValidity(areaConflictNormalised,
+##                              normalised =FALSE,
+##                              getInvalidData = TRUE)
+##
+
+## we realise that there are still some invalid flag combinations
+
+## empty empty ---> M u
+## Flag (E, p) --> (E, f)
+## Flag (E, e) --> (I, e)
+
+autoFlagCorrection = function(data,
+                              value= "Value",
+                              flagObservationStatusVar = "flagObservationStatus",
+                              flagMethodVar = "flagMethod"){
+  dataCopy = copy(data)
+  
+  ## Correction (): (, ) --> (M, u)
+  
+  
+  correctionFilter =
+    dataCopy[[flagObservationStatusVar]] == "" &
+    dataCopy[[flagMethodVar]] == ""
+  
+  dim2=dim(dataCopy[correctionFilter,])
+  message("Number of (E, t) replaced with (E, -)", dim2[1])
+  
+  
+  dataCopy[correctionFilter,
+           `:=`(c(value,flagObservationStatusVar, flagMethodVar),
+                .(NA_real_,"M", "u"))]
+  
+  ## Correction (2): (E, t) --> (E, -)
+  
+  
+  correctionFilter =
+    dataCopy[[flagObservationStatusVar]] == "E" &
+    dataCopy[[flagMethodVar]] == "t"
+  
+  dim2=dim(dataCopy[correctionFilter,])
+  message("Number of (E, t) replaced with (E, -)", dim2[1])
+  
+  
+  
+  dataCopy[correctionFilter,
+           `:=`(c(flagObservationStatusVar, flagMethodVar),
+                .("E", "-"))]
+  
+  ## Correction (3): (E, e) --> (I, e)
+  correctionFilter =
+    dataCopy[[flagObservationStatusVar]] == "E" &
+    dataCopy[[flagMethodVar]] == "e"
+  
+  
+  dim3=dim(dataCopy[correctionFilter,])
+  message("Number of (E, e) replaced with (I, e)", dim3[1])
+  
+  dataCopy[correctionFilter,
+           `:=`(c(flagObservationStatusVar, flagMethodVar),
+                .("I", "e"))]
+  
+  ## Correction (4): (E, p) --> (E, f)
+  correctionFilter =
+    dataCopy[[flagObservationStatusVar]] == "E" &
+    dataCopy[[flagMethodVar]] == "p"
+  
+  
+  dim4=dim(dataCopy[correctionFilter,])
+  message("Number of (E, p) replaced with (E, f)", dim4[1])
+  
+  
+  dataCopy[correctionFilter,
+           `:=`(c(flagObservationStatusVar, flagMethodVar),
+                .("E", "f"))]
+  
+  dataCopy
+  
+  ##   data.table(dim1[1],dim2[1],dim3[1],dim4[1])
+  
+  
+}
+areaConflictNormalised=autoFlagCorrection(areaConflictNormalised)
+
+ensureFlagValidity(areaConflictNormalised,
+                   normalised =TRUE,
+                   getInvalidData = FALSE,
+                   returnData = FALSE) 
+ 
+
+areaCleaned=denormalise(areaConflictNormalised, denormaliseKey = "measuredElement")
+
+## ensureCorrectMissingValue(normalise(areaConflict), getInvalidData = TRUE)
+  
+imputedArea=imputeAreaSown(data = areaCleaned,
+                             byKey= c("geographicAreaM49","measuredItemCPC"))
+  
+  
+  
+  ## AreaSown must be greater then or at least equal to area harvested. Check that in the
+  ## AreaSown column all the values are not lower than AreaHarvested
+  
+  ensureNoConflictingAreaSownHarvested (data= imputedArea,
+                                        valueColumn1= "Value_measuredElement_5025",
+                                        valueColumn2= "Value_measuredElement_5312",
+                                        returnData = FALSE,
+                                        normalised = FALSE,
+                                        getInvalidData = FALSE)
+  
+
+ ensureFlagValidity(imputedArea,
+                   normalised =FALSE,
+                   getInvalidData = TRUE)
+
+
   
   
   
   
 
+  climate = getWorldBankClimateData()  
+    
+  
 
-  
-  
   
   finalModelData = mergeAllSeedData(seedData = seed, imputedArea, climate)
+  
+  
+## Extrapolate climateData: it means to fill the emply cells with the average temperature
+  
+   
+  meanTEMP=aggregate(finalModelData[!is.na(Value_wbIndicator_SWS.FAO.TEMP),Value_wbIndicator_SWS.FAO.TEMP],
+             by=list(finalModelData[!is.na(Value_wbIndicator_SWS.FAO.TEMP),geographicAreaM49]),FUN="mean")
+  colnames(meanTEMP)=c("geographicAreaM49","Value_wbIndicator_SWS.FAO.TEMP_mean")
+  finalModelData=merge(finalModelData, meanTEMP, by="geographicAreaM49")
+  finalModelData[is.na(Value_wbIndicator_SWS.FAO.TEMP) ,
+                 Value_wbIndicator_SWS.FAO.TEMP :=  Value_wbIndicator_SWS.FAO.TEMP_mean]
+  
+  finalModelData[,Value_wbIndicator_SWS.FAO.TEMP_mean:=NULL]
+  
+ 
+  
+
   finalModelData = finalModelData[Value_measuredElement_5525 > 1 &
                                       Value_measuredElement_5025 >1, ]
   ## We have to remove cases where we do not have temperature, as we cannot create
   ## a model when independent variables are missing.  The predictions would be NA
   ## anyways, and so we wouldn't be saving anything to the database if we left
   ## them in.
+  
+  
+
+  
+  
   finalModelData = finalModelData[!is.na(Value_wbIndicator_SWS.FAO.TEMP), ]
+  
+ 
+  set.seed(260716)
   
   # Michael's model
   seedLmeModel = 
@@ -208,21 +343,58 @@ climate= getWorldBankClimateData()
 # par(mfrow=c(1,1))
 # qqnorm(residuals(lossLmeModel))
 # qqline(residuals(lossLmeModel))
-}
 
-seed = getSelectedSeedData()
+  
+  
 
-seed = removeCarryForward(data = seed, variable = "Value_measuredElement_5525")
-seed = buildCPCHierarchy(data = seed, cpcItemVar = itemVar, levels = 3)
 
-finalPredictData = mergeAllSeedData(seedData = seed, imputedArea, climate)
-finalPredictData = finalPredictData[Value_measuredElement_5525 > 1 &
-                                        Value_measuredElement_5025 > 1, ]
+seed1 = getSelectedSeedData()
+
+seed1= preProcessing(data= seed1, normalised = FALSE)
+
+seed1 = removeCarryForward(data = seed11, variable = "Value_measuredElement_5525")
+
+
+
+
+##seedRemoved=removeNonProtectedFlag(seed,normalised = FALSE)
+
+ensureFlagValidity(seed1,normalised = FALSE,returnData = FALSE, getInvalidData = TRUE)
+
+
+
+seed1 = removeCarryForward(data = seed1, variable = "Value_measuredElement_5525")
+seed1 = buildCPCHierarchy(data = seed1, cpcItemVar = itemVar, levels = 3)
+
+finalPredictData = mergeAllSeedData(seedData = seed1, imputedArea, climate)
+
+
+
+
+
 ## We have to remove cases where we do not have temperature, as we cannot create
 ## a model when independent variables are missing.  The predictions would be NA
 ## anyways, and so we wouldn't be saving anything to the database if we left
 ## them in.
-finalPredictData = finalPredictData[!is.na(Value_wbIndicator_SWS.FAO.TEMP), ]
+
+meanTEMP1=aggregate(finalPredictData[!is.na(Value_wbIndicator_SWS.FAO.TEMP),Value_wbIndicator_SWS.FAO.TEMP],
+                    by=list(finalPredictData[!is.na(Value_wbIndicator_SWS.FAO.TEMP),geographicAreaM49]),FUN="mean")
+
+colnames(meanTEMP1)=c("geographicAreaM49","Value_wbIndicator_SWS.FAO.TEMP_mean")
+
+finalPredictData=merge(finalPredictData, meanTEMP1, by="geographicAreaM49")
+finalPredictData=finalPredictData[is.na(Value_wbIndicator_SWS.FAO.TEMP) ,
+                                  Value_wbIndicator_SWS.FAO.TEMP :=  Value_wbIndicator_SWS.FAO.TEMP_mean]
+
+
+
+
+
+finalPredictData[,Value_wbIndicator_SWS.FAO.TEMP_mean:=NULL]
+
+
+
+##finalPredictData = finalPredictData[!is.na(Value_wbIndicator_SWS.FAO.TEMP), ]
 
 
 ## Impute selected data
@@ -230,39 +402,47 @@ finalPredictData = finalPredictData[!is.na(Value_wbIndicator_SWS.FAO.TEMP), ]
 finalPredictData[, predicted := exp(predict(seedLmeModel,
                                             newdata = finalPredictData,
                                             allow.new.levels = TRUE))]
-# ggplot(finalPredictData, aes(x = Value_measuredElement_5525, y = predicted)) +
-#   geom_point()
-
-
-finalPredictData= preProcessing(data= finalPredictData,
-                                normalised = FALSE)
-
-finalPredictData=removeManuallyEstimation(finalPredictData,
-                          valueVar= "Value_measuredElement_5525",
-                          observationFlagVar= "flagObservationStatus_measuredElement_5525",
-                          methodFlagVar= "flagMethod_measuredElement_5525")
 
 
 
 
-finalPredictData=removeImputationEstimation(finalPredictData,
-                       valueVar= "Value_measuredElement_5525",
-                       observationFlagVar= "flagObservationStatus_measuredElement_5525",
-                       methodFlagVar= "flagMethod_measuredElement_5525")
+finalPredictData[,flagComb:=paste(flagObservationStatus_measuredElement_5525,
+                                  flagMethod_measuredElement_5525,sep=";")]
+
+protected=flagValidTable[Protected==TRUE,]
+protected[,comb:=paste(flagObservationStatus,flagMethod,sep=";")]
+
+tobeOverwritten=!finalPredictData[,flagComb] %in% protected[,comb]
 
 
 
+## This step should embed the idea that just non protected flag combinations shoul be overwritten
+## while the previously version (the one now commented) was overwriting all the item characterised by observationFlag
+## equal to c("E", "I", "T"). The only exception in the Seed context is the (M,-) flag combination: 
+## it has to be overwritten if it exists an areaSown different from (tht's why we embeded into the conditions to select the
+## items to be overwritten: !is.na(Value_measuredElement_5025))
+##
+##
+## finalPredictData[(is.na(Value_measuredElement_5525) |
+##                  flagObservationStatus_measuredElement_5525 %in% c("E", "I", "T")) &
+##                 !is.na(predicted),
+##             `:=` (c("Value_measuredElement_5525",
+##                     "flagObservationStatus_measuredElement_5525",
+##                     "flagMethod_measuredElement_5525"),
+##                 list(predicted, "I", "e"))]
 
-finalPredictData[(is.na(Value_measuredElement_5525) |
-                  flagObservationStatus_measuredElement_5525 %in% c("M")) &
-                 !is.na(predicted),
-             `:=` (c("Value_measuredElement_5525",
-                     "flagObservationStatus_measuredElement_5525",
-                     "flagMethod_measuredElement_5525"),
-                 list(predicted, "I", "e"))]
+
+finalPredictData[(is.na(Value_measuredElement_5525) |tobeOverwritten) &
+                   !is.na(predicted)&
+                   !is.na(Value_measuredElement_5025),
+                 `:=` (c("Value_measuredElement_5525",
+                         "flagObservationStatus_measuredElement_5525",
+                         "flagMethod_measuredElement_5525"),
+                       list(predicted, "I", "e"))]
+
+
 
 finalPredictData[, predicted := NULL]
-
 
 
 ##Output data validation 
@@ -285,22 +465,34 @@ ensureFlagValidity(data = finalPredictData,
 
 
 
-finalPredictData[, .(geographicAreaM49, timePointYears, measuredItemCPC,
-                     Value_measuredElement_5525, flagObservationStatus_measuredElement_5525, 
-                     flagMethod_measuredElement_5525)] %>%
+finalPredictData_imputed=finalPredictData[, .(geographicAreaM49, timePointYears, measuredItemCPC,
+                                              Value_measuredElement_5525,flagObservationStatus_measuredElement_5525, 
+                                              flagMethod_measuredElement_5525)] %>%
   mutate(timePointYears = as.character(timePointYears)) %>%
   filter(., (flagObservationStatus_measuredElement_5525 == "I" & 
-               flagMethod_measuredElement_5525 == "e")) %>%
-  ensureProtectedData(data = .,
-                                         domain = "agriculture",
-                                         dataset = "aproduction",
-                                         normalised = FALSE,
-                                         returnData = FALSE,
-                                         getInvalidData = FALSE
-                                         )
+               flagMethod_measuredElement_5525 == "e")) 
 
 
 
-saveSeedData(data = finalPredictData)
+finalPredictData_imputed= normalise(finalPredictData_imputed)
+
+## This function cannot be run anymore because some (M,-) flag combinations have been overwritten.
+##In particular the (M,-) values for seed use for which we have an area sown different from NA
+##  
+## ensureProtectedData(data = finalPredictData_imputed,
+##                     domain = "agriculture",
+##                     dataset = "aproduction",
+##                     normalised = TRUE,
+##                     returnData = FALSE,
+##                     getInvalidData = TRUE
+## )
+## 
+
+SaveData(domain = sessionKey@domain,
+         dataset = sessionKey@dataset,
+         data = finalPredictData_imputed)
+
+
+
 
 "Module finished successfully"
